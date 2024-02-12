ab1f086b67f01d486ce19bab219320299ceb3db8
Rustam0220 <rustam20021015@gmail.com>
2024-02-13 00:06:18 +0600

    hw1

--- product/views.py
+++ product/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
 from datetime import datetime
 
-
 def hello_view(request):
     return HttpResponse("Hello! It's my project")
-
 
 def current_date_view(request):
     current_date = datetime.now().strftime("%Y-%m-%d")
     return HttpResponse("Current date: " + current_date)
-
 
 def goodbye_view(request):
     return HttpResponse("Goodbye user!")
